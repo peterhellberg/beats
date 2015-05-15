8ece0ae560e719589738a75fac560d180d491168
Peter Hellberg <peter@c7.se>
2015-05-15 10:23:25 +0200

    FromTime example, changed package to beats_test

--- beats_test.go
+++ beats_test.go
@@ -1,14 +1,17 @@
-package beats
+package beats_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
+
+	"github.com/peterhellberg/beats"
 )
 
 func TestBeatsString(t *testing.T) {
 	in, out := 14, "@014"
 
-	if got := String(in); got != out {
+	if got := beats.String(in); got != out {
 		t.Errorf("String(%v) = %v, want %v", in, got, out)
 	}
 }
@@ -16,16 +19,21 @@ func TestBeatsString(t *testing.T) {
 func TestBeatsFromTime(t *testing.T) {
 	in, out := time.Date(2014, 9, 2, 9, 23, 20, 0, time.UTC), 432
 
-	if got := FromTime(in); got != out {
+	if got := beats.FromTime(in); got != out {
 		t.Errorf("FromTime(%v) = %v, want %v", in, got, out)
 	}
+}
+
+func ExampleFromTime() {
+	fmt.Println(beats.FromTime(time.Date(2015, 5, 14, 12, 35, 43, 0, time.UTC)))
+	// Output: 566
 }
 
 func TestBeatsBMT(t *testing.T) {
 	in, out := time.Date(2012, 4, 11, 16, 45, 51, 0, time.UTC),
 		time.Date(2012, 4, 11, 17, 45, 51, 0, time.UTC)
 
-	if got := BMT(in); got != out {
+	if got := beats.BMT(in); got != out {
 		t.Errorf("BMT(%v) = %v, want %v", in, got, out)
 	}
 }
